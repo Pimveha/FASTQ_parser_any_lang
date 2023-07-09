3a18a84f62a15b579f89dc6120e39100c270f2af
Pim <vanhelvoortpim@gmail.com>
2023-07-09 17:27:35 +0200

    read file + added fastq table

--- Lua/lua_fastq_parser.lua
+++ Lua/lua_fastq_parser.lua
@@ -1,7 +1,40 @@
-function FastqRecord(name, sequence, score)        -- "Point" object constructor
-    return { name = name, sequence = sequence, score = score }   -- Creates and returns a new object (table)
+local open = io.open
+-- local inspect = require('inspect')
+
+function FastqRecord(name, sequence, quality)
+    return { name = name, sequence = sequence, quality = quality }
   end
 
-array = { FastqRecord("a", "ATGC", "ABCD"), FastqRecord("b", "tTGC", "QBCD") }
+function read_file(path)
+    local file = open(path, "rb") 
+    if not file then return nil end
+    local lines = {}
+    for line in file:lines() do 
+        lines[#lines + 1] = line
+      end
+    file:close()
+    return lines
+  end
 
-print(array[1].name)
+-- array = { FastqRecord("a", "ATGC", "ABCD"), FastqRecord("b", "tTGC", "QBCD") }
+
+-- print(array[1].name)
+
+lines = read_file('../example.fastq')
+-- print(lines[2])
+
+print(#lines)
+
+FastqTable = {}
+for i = 1, #lines, 4 do
+    name = lines[i]
+    sequence = lines[i+1]
+    quality = lines[i+3]
+    -- FastqRecord(name, sequence, quality)
+    -- table.insert(FastqTable, FastqRecord(name, sequence, quality))
+    FastqTable[#FastqTable+1] = FastqRecord(name, sequence, quality)
+  end
+
+
+-- print(FastqTable)
+-- print(inspect(FastqTable))
