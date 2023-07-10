d8c0ca32d7cb76a99aa91494218683e9481a6978
Pim <vanhelvoortpim@gmail.com>
2023-07-10 14:27:10 +0200

    fastq content placeed in procesContent()

--- Java/ParseFastq.java
+++ Java/ParseFastq.java
@@ -3,12 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
-class files {
-    public ArrayList<String>() read_fastq(String path_name) {
+class ParseFastq {
+    public void read_fastq(String path_name) {
         // String path_name = path_name;
-        lines = new ArrayList<String>();
+        // Map<String, String> fastqMap = new HashMap<String, String, String>();
         try {
             // File move_file = new File("./path_nam.txt");
+            String name = "";
+            String sequence = "";
+            String quality = "";
             File move_file = new File(path_name);
             Scanner myReader = new Scanner(move_file);
 
@@ -19,20 +22,27 @@ class files {
                     sequence = myReader.nextLine();
                     myReader.nextLine(); // +
                     quality = myReader.nextLine();
-
-                    
                     }
-                lines.add(line);
+                
                 // System.out.println(line.split("\t"));
+                procesContent(name, sequence, quality);
                 }
             }
         catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        return lines;
+    }
+    public void procesContent(String name, String sequence, String quality) {
+        System.out.println(name);
+        System.out.println(sequence);
+        System.out.println(quality);
+        System.out.println("---------------------------");
+    }
+
+    public static void main(String[] args) {
+        String filePath = "../example.fastq";
+        ParseFastq parser = new ParseFastq();
+        parser.read_fastq(filePath);
     }
 }
-
-
-class fastq
