45b257ca2a976eba19c53cb1c87c3915c89d6485
Pim <vanhelvoortpim@gmail.com>
2023-07-11 16:34:00 +0200

    dataframe parsing check (primative)

--- R/r_fastq_parser.R
+++ R/r_fastq_parser.R
@@ -1,29 +1,27 @@
 lines <- readLines("../example.fastq")
 
 content_list = c("name", "sequence", "ignore", "quality")
-length(rep(content_list, times = length(lines)/4))
-length(lines)
-length(rep(1:(length(lines)/4), each = 4))
-# seq(0, length(lines), by=4) 
-# rep(1:4, c(4,4,4,4))
-# 
-# rep_times = rep(4, length(lines)/4)
-# ?rep
-# 
-# rep(1:4, each = 4)
-# 
-# rep(1:length(lines), each = 4)
-# rep(1:length(lines), 4)
-# rep(1:length(lines), c(4,4,4,4))
+# length(rep(content_list, times = length(lines)/4))
+# length(lines)
+# length(rep(1:(length(lines)/4), each = 4))
 ?seq
 
 
-df_lines <- data.frame(fastq_content = lines,
+fastq_df <- data.frame(fastq_content = lines,
                        id = rep(1:(length(lines)/4), each = 4),
                        id_content = rep(content_list, times = length(lines)/4))
+# fastq_df[fastq_df$id == 2 & fastq_df$id_content == "name" ,]$fastq_content
+# startsWith(fastq_df[fastq_df$id == 2 & fastq_df$id_content == "name" ,]$fastq_content, "@") == TRUE
+# fastq_df[fastq_df$id == length(lines)/4 & fastq_df$id_content == "name" ,]$fastq_content
+if (startsWith(fastq_df[fastq_df$id == length(lines)/4 & fastq_df$id_content == "name" ,]$fastq_content, "@")){
+  cat("fastq file was most likely parsed correctly\n\nyou can now use the fastq_df (dataframe).\n examples:\n   fastq_df[fastq_df$id == 1,]\n   fastq_df[fastq_df$id_content == 'name,]")
+}else {
+  print("parsing failed")
+}
 
-df_lines[df_lines$id == 1,]
+fastq_df[fastq_df$id == 1,]
+fastq_df[fastq_df$id_content == "name",]
 
-# rownames(df_lines) <- c()
+# rownames(fastq_df) <- c()
   
 # paste0(rep(mynames,length.out=6587),rep(1:366,each=18,length.out=6587))
