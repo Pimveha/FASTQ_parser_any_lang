1693b54333c65d93df03cf44a050ff69f5bf887c
Pim <vanhelvoortpim@gmail.com>
2023-07-11 16:19:35 +0200

    dataframe working

--- R/r_fastq_parser.R
+++ R/r_fastq_parser.R
@@ -3,20 +3,26 @@ lines <- readLines("../example.fastq")
 content_list = c("name", "sequence", "ignore", "quality")
 length(rep(content_list, times = length(lines)/4))
 length(lines)
-seq(0, length(lines), by=4) 
-rep(1:4, c(4,4,4,4))
-
-rep_times = rep(4, length(lines)/4)
-
-rep(1:length(lines), c(4,4,4,4))
+length(rep(1:(length(lines)/4), each = 4))
+# seq(0, length(lines), by=4) 
+# rep(1:4, c(4,4,4,4))
+# 
+# rep_times = rep(4, length(lines)/4)
+# ?rep
+# 
+# rep(1:4, each = 4)
+# 
+# rep(1:length(lines), each = 4)
+# rep(1:length(lines), 4)
 # rep(1:length(lines), c(4,4,4,4))
 ?seq
 
 
 df_lines <- data.frame(fastq_content = lines,
-                       id = seq(0, length(lines), by=4),
+                       id = rep(1:(length(lines)/4), each = 4),
                        id_content = rep(content_list, times = length(lines)/4))
-df_lines$lines
+
+df_lines[df_lines$id == 1,]
 
 # rownames(df_lines) <- c()
   
